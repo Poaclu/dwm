fecb2c3a9c70cb0170b684b7d9bea7e6c4e4e595
Poaclu <25772718+Poaclu@users.noreply.github.com>
2023-06-28 07:20:33 +0200

    changed default browser to brave

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray
 static const char *termcmd[]  = { "st", NULL };
 static const char *kittycmd[] = { "kitty", NULL };
 static const char *lock[]     = { "slock", NULL };
-static const char *browser[]  = { "librewolf", NULL};
+static const char *browser[]  = { "brave", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
